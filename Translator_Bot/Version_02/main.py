7df5f16b4e7fcf02ca9eda453233f745cc5b1922
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-07-13 09:49:57 +0500

    done

--- Translator_Bot/Version_02/main.py
+++ Translator_Bot/Version_02/main.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from deep_translator import GoogleTranslator
 
 load_dotenv()
 TOKEN = os.getenv("API_TOKEN")
@@ -16,12 +15,6 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!")
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    translated = GoogleTranslator(source='auto', target='uz').translate(message.text)
-    await message.answer(translated)
 
 
 if __name__ == '__main__':
