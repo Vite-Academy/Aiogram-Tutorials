c7514124faf8f7ad33ac516b95a487e57b876d77
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-07-14 10:13:37 +0500

    done

---
+++ Starter/main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+load_dotenv(".env")
+TOKEN = os.getenv("API_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token = TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!")
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
