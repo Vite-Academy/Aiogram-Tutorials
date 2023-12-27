56d0d7ead492694e9dba08f8bbfae05a7168ce12
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-12-27 23:35:05 +0500

    done

--- Wikipedia_Bot/main.py
+++ Wikipedia_Bot/main.py
@@ -1,25 +1,27 @@
-import os
-from dotenv import load_dotenv
+import asyncio
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+import sys
+from os import getenv
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
 import wikipedia
 
 load_dotenv()
-TOKEN = os.getenv("API_TOKEN")
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token = TOKEN)
-dp = Dispatcher(bot)
+TOKEN = getenv("API_TOKEN")
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+@dp.message(CommandStart())
+async def send_welcome(message: Message):
     await message.reply("Hi!")
 
 
-@dp.message_handler()
-async def sendWiki(message: types.Message):
+@dp.message()
+async def sendWiki(message: Message):
     try:
         respond = wikipedia.summary(message.text)
         await message.answer(respond)
@@ -27,5 +29,11 @@ async def sendWiki(message: types.Message):
         await message.answer("Not found. 😔")
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+async def main() -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
